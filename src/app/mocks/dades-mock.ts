bffeaf81f8beae5da2e90e9507c61d2dde614d9d
bielEneriz <bieleneriztello@gmail.com>
2026-03-26 12:23:06 +0100

    Update dades-mock.ts

---
+++ src/app/mocks/dades-mock.ts
@@ -0,0 +1,14 @@
+import { Element } from '../models/element.model';
+
+export const ELEMENTS_MOCK: Element[] = [
+  { id: 1, nom: 'Element 1', valor: 10, descripcio: 'Descripció 1' },
+  { id: 2, nom: 'Element 2', valor: 20 },
+  { id: 3, nom: 'Element 3', valor: 30 },
+  { id: 4, nom: 'Element 4', valor: 40 },
+  { id: 5, nom: 'Element 5', valor: 50 },
+  { id: 6, nom: 'Element 6', valor: 60 },
+  { id: 7, nom: 'Element 7', valor: 70 },
+  { id: 8, nom: 'Element 8', valor: 80 },
+  { id: 9, nom: 'Element 9', valor: 90 },
+  { id: 10, nom: 'Element 10', valor: 100 }
+];
